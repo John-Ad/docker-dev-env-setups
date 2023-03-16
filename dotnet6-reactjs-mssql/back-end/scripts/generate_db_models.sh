3e49140fc751cd981b5b0685a9b6059ff92c41a7
John-Ad <adriaanscorne@gmail.com>
2023-03-16 06:21:40 +0200

    added basic db tables and generated models

--- dotnet6-reactjs-mssql/back-end/scripts/generate_db_models.sh
+++ dotnet6-reactjs-mssql/back-end/scripts/generate_db_models.sh
@@ -1,38 +1,13 @@
-#   Script to generate DB Models
-#   
-#   Removes all cs files in the DB_Models folder
-#   Removes context file in Helpers folder
-#   Generates Models and stores them in DB_Models folder
-#   Generates context file and stores it in Helpers folder
-#
-
 cd ..
 
-clear
-echo Enter DB username:
-read uname
-clear
-echo Enter DB password:
-read pword
-clear
-
-conn="server=localhost; port=3307; database=SCA_ITS; user=$uname; password=$pword; Persist Security Info=False; Connect Timeout=300"
+conn="Server=localhost; Database=TestDB; User=sa; Password=BCity@123; TrustServerCertificate=True;"
 
 echo "Generating Models..."
 
-echo dotnet-ef dbcontext scaffold --force \"$conn\" Pomelo.EntityFrameworkCore.MySql -o DB_Models | bash
+echo dotnet-ef dbcontext scaffold --force \"$conn\" Microsoft.EntityFrameworkCore.SqlServer -o DB_Models | bash
 
 status=$?       # get return status
 
-if [ $status -eq 0 ]    # 0 == success
-then
-    echo 'Successfully generated files'
-
-    rm ./Helpers/SCA_ITSContext.cs
-    mv ./DB_Models/SCA_ITSContext.cs ./Helpers/
-else
-    echo 'Failed to generate files'
-fi
 
 
 
