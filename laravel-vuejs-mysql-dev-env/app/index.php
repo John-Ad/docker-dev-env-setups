c48c8cf30e15e70d419d302b076331083fa801bf
John-Ad <adriaanscorne@gmail.com>
2023-05-03 14:25:27 +0200

    added script to enter app docker shell

--- laravel-vuejs-mysql-dev-env/app/index.php
+++ laravel-vuejs-mysql-dev-env/app/index.php
@@ -9,3 +9,41 @@ $message = match ($status) {
 };
 
 echo $message;
+
+echo "<br>";
+
+
+
+/**
+ * A simple user class
+ *
+ * @property string $name
+ * @property int $age
+ */
+class User
+{
+    public string $name = "John Doe";
+    public int $age = 30;
+
+    // constructor
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age  = $age;
+    }
+
+    // toString
+    public function __toString(): string
+    {
+        return "User {$this->name} is {$this->age} years old";
+    }
+}
+
+$user = new User("John Doe", 30);
+
+$user->name = "Jane Doe";
+
+// convert user to string
+$userString = (string) $user;
+
+echo $user;
