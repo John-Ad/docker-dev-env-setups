adb58d8de94d4ed78e680bf903442a561b824ab3
John-Ad <adriaanscorne@gmail.com>
2023-05-26 21:07:46 +0200

    updated navbar to show certain links when authed and hide them when not

--- svelte-pocketbase/app/src/components/Navbar.svelte
+++ svelte-pocketbase/app/src/components/Navbar.svelte
@@ -1,15 +1,32 @@
 <script lang="ts">
-    import { link } from "svelte-spa-router";
+    import { link, push } from "svelte-spa-router";
+    import pb, { isAuthenticated } from "../api/pb";
+
+    let isLoggedIn = pb.authStore.isValid;
+
+    isAuthenticated.subscribe((value) => {
+        isLoggedIn = value;
+    });
+
+    const logout = async () => {
+        await pb.authStore.clear();
+        push("/login");
+    };
 </script>
 
 <nav>
     <ul>
-        <li><strong>Brand</strong></li>
+        <li><h1><strong>Brand</strong></h1></li>
     </ul>
     <ul>
         <li><a href="/" use:link>Home</a></li>
-        <li><a href="/login" use:link>login</a></li>
-        <li><a href="/register" use:link>register</a></li>
+
+        {#if !isLoggedIn}
+            <li><a href="/login" use:link>login</a></li>
+            <li><a href="/register" use:link>register</a></li>
+        {:else}
+            <li><button on:click={logout}>Logout</button></li>
+        {/if}
     </ul>
 </nav>
 
@@ -18,5 +35,9 @@
         padding-left: 3rem;
         padding-right: 3rem;
         border-bottom: 1px solid #ccc;
+    }
+    h1 {
+        font-size: 2rem;
+        margin: 0;
     }
 </style>
