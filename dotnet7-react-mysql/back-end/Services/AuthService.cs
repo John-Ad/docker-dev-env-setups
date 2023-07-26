289dafe312951591a29de16ffe8d79398cc7c281
John-Ad <adriaanscorne@gmail.com>
2023-07-26 07:22:10 +0200

    added login method

--- dotnet7-react-mysql/back-end/Services/AuthService.cs
+++ dotnet7-react-mysql/back-end/Services/AuthService.cs
@@ -1,5 +1,6 @@
 using back_end.Data;
 using back_end.Models;
+using back_end.Utilities;
 using Microsoft.EntityFrameworkCore;
 
 namespace back_end.Services;
@@ -7,10 +8,12 @@ namespace back_end.Services;
 public class AuthService
 {
     private readonly ApplicationDBContext _context;
+    private readonly IConfiguration _configuration;
 
-    public AuthService(ApplicationDBContext context)
+    public AuthService(ApplicationDBContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
 
     public async Task<Result> Register(User user)
@@ -37,6 +40,64 @@ public class AuthService
             result.statusCode = 201;
             result.message = "User created successfully";
             result.data = user;
+            return result;
+        }
+        catch (Exception e)
+        {
+            result.statusCode = 500;
+            result.message = e.Message;
+            return result;
+        }
+    }
+
+    // create login method
+    public async Task<Result> Login(User user)
+    {
+        var result = new Result
+        {
+            statusCode = 0,
+            message = "",
+            data = ""
+        };
+        try
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            if (existingUser == null)
+            {
+                result.statusCode = 404;
+                result.message = "User does not exist";
+                return result;
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(user.Password, existingUser.Password))
+            {
+                result.statusCode = 400;
+                result.message = "Invalid credentials";
+                return result;
+            }
+
+            if (!existingUser.EmailConfirmed)
+            {
+                result.statusCode = 400;
+                result.message = "Please confirm your email";
+                return result;
+            }
+
+            var jwtHandler = new JwtHandler(_configuration);
+            var token = jwtHandler.generate(existingUser.Id, existingUser.RoleId);
+            var refreshToken = jwtHandler.generateRefreshToken();
+
+            existingUser.RefreshToken = refreshToken;
+            _context.Users.Update(existingUser);
+            await _context.SaveChangesAsync();
+
+            result.statusCode = 200;
+            result.message = "User logged in successfully";
+            result.data = new LoginResponse
+            {
+                token = token,
+                refreshToken = refreshToken
+            };
             return result;
         }
         catch (Exception e)
